82092321804e996779a7eb05676d3ea0048fe6f6
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2021-08-31 09:49:03 +0900

    Initial Commit

---
+++ EditListSample2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EditListSample2
+//
+//  Created by Yusuke Hasegawa on 2021/08/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EditListSample2/EditListSample2App.swift
@@ -0,0 +1,17 @@
+//
+//  EditListSample2App.swift
+//  EditListSample2
+//
+//  Created by Yusuke Hasegawa on 2021/08/31.
+//
+
+import SwiftUI
+
+@main
+struct EditListSample2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
